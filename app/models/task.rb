39c5f034cebc43d9e36ea8708b7a91158915c223
Tomasz Biłas <tomasz@bilas.pl>
2017-10-27 22:46:53 +0200

    add deadline validator to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validate :deadline_valid
+
+  private
+
+  def deadline_valid
+    return if deadline.nil? || deadline.is_a?(Time)
+    errors.add(:deadline, 'value is invalid')
+    false
+  end
 end
